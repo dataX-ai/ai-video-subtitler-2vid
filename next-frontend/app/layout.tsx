762cbd48a583fe70a9ab8ca907b39db8e52eb505
psarathi012 <psarathi012@gmail.com>
2025-03-25 22:38:52 +0530

    Added Posthog analytics

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { PostHogProvider } from './providers'
+
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -14,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <PostHogProvider>
+          {children}
+        </PostHogProvider>
+      </body>
     </html>
   )
 }
